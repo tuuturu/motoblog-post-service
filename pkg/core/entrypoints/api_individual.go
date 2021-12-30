09c7d7deda8d00da336b89abc15bd8c9ff140583
Julius Pedersen <deifyed@tutanota.com>
2021-12-30 10:21:04 +0100

    ✅ Add individual entrypoint functionality

--- pkg/core/entrypoints/api_individual.go
+++ pkg/core/entrypoints/api_individual.go
@@ -1,8 +1,13 @@
 package entrypoints
 
 import (
+	"bytes"
+	"errors"
+	"io"
+	"log"
 	"net/http"
 
+	"github.com/deifyed/post-service/pkg/models"
 	"github.com/deifyed/post-service/pkg/stores"
 	"github.com/gin-gonic/gin"
 )
@@ -10,20 +15,118 @@ import (
 // DeletePost -
 func DeletePost(postStore stores.PostStore, contentStore stores.ContentStore) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{})
+		id := c.Param("id")
+
+		err := postStore.DeletePost(id)
+		if err != nil {
+			log.Printf("unable to delete post: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		err = contentStore.DeleteContent(id)
+		if err != nil {
+			log.Printf("unable to delete content: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		c.Status(http.StatusNoContent)
 	}
 }
 
 // GetPost -
 func GetPost(postStore stores.PostStore, contentStore stores.ContentStore) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{})
+		id := c.Param("id")
+
+		post, err := postStore.GetPost(id)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		content, err := contentStore.RetrieveContent(id)
+		if err != nil {
+			status := http.StatusInternalServerError
+
+			if errors.Is(err, stores.ErrNotFound) {
+				status = http.StatusNotFound
+			}
+
+			c.Status(status)
+
+			return
+		}
+
+		raw, err := io.ReadAll(content)
+		if err != nil {
+			log.Printf("Error buffering content: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		post.Content = string(raw)
+
+		c.JSON(http.StatusOK, post)
 	}
 }
 
 // UpdatePost -
 func UpdatePost(postStore stores.PostStore, contentStore stores.ContentStore) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{})
+		var updatedPost models.Post
+
+		err := c.Bind(&updatedPost)
+		if err != nil {
+			log.Printf("Error binding post: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		originalPost, err := postStore.GetPost(updatedPost.Id)
+		if err != nil {
+			log.Printf("Error retrieving original post: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		updatePost(&originalPost, updatedPost)
+
+		err = contentStore.StoreContent(
+			originalPost.Id,
+			bytes.NewReader([]byte(originalPost.Content)),
+		)
+		if err != nil {
+			log.Printf("Error updating content: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		originalPost.Content = truncateContent(originalPost.Content)
+
+		err = postStore.UpdatePost(originalPost.Id, originalPost)
+		if err != nil {
+			log.Printf("Error updating post: %s", err.Error())
+
+			c.Status(http.StatusInternalServerError)
+
+			return
+		}
+
+		c.JSON(http.StatusOK, originalPost)
 	}
 }
--- pkg/core/entrypoints/helpers.go
+++ pkg/core/entrypoints/helpers.go
@@ -1,5 +1,9 @@
 package entrypoints
 
+import (
+	"github.com/deifyed/post-service/pkg/models"
+)
+
 const maxTruncatedLength = 160
 
 func truncateContent(original string) string {
@@ -8,4 +12,24 @@ func truncateContent(original string) string {
 	}
 
 	return original
+}
+
+func merge(a string, b string) string {
+	if a == b {
+		return a
+	}
+
+	if len(b) > 0 {
+		return b
+	}
+
+	return a
+}
+
+func updatePost(target *models.Post, source models.Post) error {
+	target.Title = merge(target.Title, source.Title)
+	target.Time = merge(target.Time, source.Time)
+	target.Content = merge(target.Content, source.Content)
+
+	return nil
 }
