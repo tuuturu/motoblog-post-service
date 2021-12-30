62d31e0415defef7221b84f6c5c4fe20b77c840c
Julius Pedersen <deifyed@tutanota.com>
2021-12-30 11:56:57 +0100

    ✅ Add test for get all posts

--- tests/api_collection_test.go
+++ tests/api_collection_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/deifyed/post-service/pkg/core"
@@ -46,11 +47,119 @@ func TestCreatePost(t *testing.T) {
 				bytes.NewReader(raw),
 			)
 
+			request.Header.Add("Content-Type", "application/json")
+
 			recorder := httptest.NewRecorder()
 
 			router.ServeHTTP(recorder, request)
 
 			assert.Equal(t, tc.expectStatus[index], recorder.Code)
+		}
+	}
+}
+
+func createPost(t *testing.T, router *gin.Engine, post models.Post) {
+	raw, err := json.Marshal(post)
+	assert.NoError(t, err)
+
+	request := httptest.NewRequest(
+		http.MethodPost,
+		"/posts",
+		bytes.NewReader(raw),
+	)
+
+	request.Header.Add("Content-Type", "application/json")
+
+	router.ServeHTTP(httptest.NewRecorder(), request)
+}
+
+func TestGetAllPosts(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		withPosts   []models.Post
+		expectPosts []models.Post
+	}{
+		{
+			name: "Should have one post",
+			withPosts: []models.Post{
+				{
+					Title:   "Dummy",
+					Content: "Dummy content",
+				},
+			},
+			expectPosts: []models.Post{
+				{
+					Title:   "Dummy",
+					Content: "Dummy content",
+				},
+			},
+		},
+		{
+			name: "Should have multiple posts",
+			withPosts: []models.Post{
+				{
+					Title:   "Dummy",
+					Content: "Dummy content",
+				},
+				{
+					Title:   "Dummy number two",
+					Content: "more Dummy content",
+				},
+				{
+					Title:   "Dummy number three",
+					Content: "even more Dummy content",
+				},
+			},
+			expectPosts: []models.Post{
+				{
+					Title:   "Dummy",
+					Content: "Dummy content",
+				},
+				{
+					Title:   "Dummy number two",
+					Content: "more Dummy content",
+				},
+				{
+					Title:   "Dummy number three",
+					Content: "even more Dummy content",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		router := core.NewRouter(core.Config{})
+
+		for _, post := range tc.withPosts {
+			createPost(t, router, post)
+		}
+
+		request := httptest.NewRequest(http.MethodGet, "/posts", nil)
+
+		recorder := httptest.NewRecorder()
+
+		router.ServeHTTP(recorder, request)
+
+		assert.Equal(t, http.StatusOK, recorder.Code)
+
+		var returnedPosts []models.Post
+
+		err := json.Unmarshal(recorder.Body.Bytes(), &returnedPosts)
+		assert.NoError(t, err)
+
+		for index := range returnedPosts {
+			assert.NotEmpty(t, returnedPosts[index].Id)
+			assert.NotEmpty(t, returnedPosts[index].Time)
+
+			returnedPosts[index].Id = ""
+			returnedPosts[index].Time = ""
+		}
+
+		for _, post := range tc.expectPosts {
+			assert.Contains(t, returnedPosts, post)
 		}
 	}
 }
